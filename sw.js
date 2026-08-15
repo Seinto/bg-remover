d9259ffd929d1dcf33ded5fd8a1b6a5006441d65
Seinto <e0627241@gmail.com>
2026-08-15 19:53:10 +0900

    Update service worker cache and assets
    Updated cache name to 'bg-remover-opt-v2' and added icon.svg to assets. Implemented cache deletion for old versions during activation.

--- sw.js
+++ sw.js
@@ -1,15 +1,24 @@
-const CACHE_NAME = 'bg-remover-opt-v1';
+const CACHE_NAME = 'bg-remover-opt-v2';
 const ASSETS = [
   './',
   './index.html',
   './app.js',
   './manifest.json',
+  './icon.svg',
   'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
   );
 });
 
